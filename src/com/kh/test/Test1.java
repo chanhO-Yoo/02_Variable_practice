24d005e9a3070c017dc6ade3aaa2c319f480bab6
Chanho <poi711@likelion.org>
2019-08-11 14:21:06 +0900

    java practice example on 20190807

---
+++ src/com/kh/test/Test1.java
@@ -0,0 +1,33 @@
+package com.kh.test;
+
+public class Test1 {
+	/*
+	 * 1. 각 자료형별 변수를 만들고 초기화한 후 그 값을 출력하는 코드를 작성해 보자.
+    	- 정수형 100, 9999억, 
+    	- 실수 486.520(float), 567.890123
+    	- 문자 A
+    	- 문자열 Hello world, 
+    	- 논리 true
+     	단, 변수를 이용하여 출력하시오 .
+	 */
+	public static void main(String[] args) {
+		int i = 100;
+		long j = 999900000000L;
+		
+		float f = 486.520f;
+		double d = 567.890123;
+		
+		char a = 'A';
+		String str = "Hello world";
+		boolean bool = true;
+
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(a);
+		System.out.println(str);
+		System.out.println(bool);
+	}
+	
+}
---
+++ src/com/kh/test/Test2.java
@@ -0,0 +1,17 @@
+package com.kh.test;
+
+public class Test2 {
+	public static void main(String[] args) {
+		String name = "Yoo Chanho";
+		int age = 28;
+		char sex = '남';
+		String phoneNum = "01012345678";
+		String email = "khac.chanc@gmail.com";
+		String address = "서울시";
+		
+		System.out.println("=============================================================================");
+		System.out.println("이름\t\t나이\t성별\t전화번호\t\t이메일\t\t\t주소");
+		System.out.println("=============================================================================");
+		System.out.printf("%s\t%d\t%c\t%s\t%s\t%s",name,age,sex,phoneNum,email,address);
+	}
+}
